b973ff26081f9e98395f5c0751b66c7bd1305a26
AKAYKO AZUMI RH <patricia@azumirh.com.br>
2026-07-12 13:23:49 -0300

    Update vite-env.d.ts

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,11 @@
 /// <reference types="vite/client" />
+
+declare namespace JSX {
+  interface IntrinsicElements {
+    "iconify-icon": React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement> & {
+      icon?: string;
+      width?: string | number;
+      height?: string | number;
+    };
+  }
+}
